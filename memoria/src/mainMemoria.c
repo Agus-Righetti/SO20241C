a463877b9ac9e1757ac9f331da9400052fe2447e
victoriamass <vickimassaini@gmail.com>
2024-04-25 19:47:07 +0000

    hilos_memoria

--- memoria/src/mainMemoria.c
+++ memoria/src/mainMemoria.c
@@ -1,33 +1,21 @@
 #include <mainMemoria.h>
 
-t_log* log_memoria;
-memoria_config* config_memoria;
-
 // void iterator(char* value) 
 // {
 // 	log_info(log_memoria, "%s", value);
 // }
 
-int main(int argc, char* argv[]) 
-{
-	decir_hola("Memoria");
-    
-	// ************* LOG Y CONFIG DE USO GENERAL *************
-    log_memoria = log_create("memoria.log", "Memoria", 1, LOG_LEVEL_DEBUG);
-    config_memoria = armar_config(log_memoria);
-    
-	// ********* SERVER PARA RECIBIR A CPU *********
-	server_para_cpu(config_memoria, log_memoria);
+int main() {
+    // ************* LOG Y CONFIG DE USO GENERAL *************
+    t_log* log_memoria = log_create("memoria.log", "Memoria", 1, LOG_LEVEL_DEBUG); 
+    memoria_config* config_memoria = armar_config(log_memoria);
 
-	// ********* SERVER PARA RECIBIR A KERNEL *********
-	server_para_kernel(config_memoria, log_memoria);
-	
-	// ********* SERVER PARA RECIBIR A I/O *********
-	server_para_io(config_memoria, log_memoria);
-	
-	//Despues podemos agregar una funcion y liberar memoria ahi
+    // ********* CREO HILOS PARA CREAR LAS CONEXIONES *********
+    crear_hilos_servidor (log_memoria, config_memoria) ;
+  
+    // ********* LIBERO TODO *********
 	log_destroy(log_memoria);
 	free(config_memoria);
-
+	
     return EXIT_SUCCESS;
 }
--- memoria/src/servidor.h
+++ memoria/src/servidor.h
@@ -5,14 +5,22 @@
 #include <utils/utilsShare.h>
 #include "configuracion.h"
 
+typedef struct {
+    memoria_config* config_memoria;
+    t_log* log_memoria;
+} thread_args;  //argumentos de los hilos
+
 // *************** DECLARACIÓN DE FUNCIONES **********
 // ********* SERVER PARA RECIBIR A CPU *********
-void server_para_cpu(memoria_config* config_memoria, t_log* log_memoria);
+void* server_para_cpu(void* args);
 
 // ********* SERVER PARA RECIBIR A KERNEL *********
-void server_para_kernel(memoria_config* config_memoria,t_log* log_memoria);
+void* server_para_kernel(void* args);
 
 // ********* SERVER PARA RECIBIR A I/O *********
-void server_para_io(memoria_config* config_memoria,t_log* log_memoria);
+void* server_para_io(void* args);
+
+// ********* CREACION DE HILOS PARA SER SERVIDOR *********
+void crear_hilos_servidor (t_log* log_memoria,memoria_config* config_memoria);
 
 #endif
