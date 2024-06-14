6665104f149fd057f9c697f1bbdfe97f28dd0af0
victoriamass <vickimassaini@gmail.com>
2024-06-14 17:18:35 +0000

    correcciones del merge

--- memoria/src/atenderCPU.h
+++ memoria/src/atenderCPU.h
@@ -22,9 +22,11 @@ void enviar_una_instruccion_a_cpu(char* instruccion);
 // **************** ACCESO A TABLA DE PAG ************
 void cpu_pide_numero_de_marco(t_buffer* un_buffer);
 int obtener_marco_segun_pagina (int pid, int nro_pag);
+void enviar_marco_consultado_a_cpu(int marco_consultado);
 
 // ******************* RESIZE ************************
 void cpu_pide_resize(t_buffer* un_buffer);
 void enviar_out_of_memory_a_cpu();
+void enviar_ok_del_resize_a_cpu();
 
 #endif
